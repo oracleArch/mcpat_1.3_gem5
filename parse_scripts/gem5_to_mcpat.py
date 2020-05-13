2043950544a72805478a1f65b2b27923b431d2cb
Anshujit <anshujitsharma@gmail.com>
2020-05-13 19:57:30 -0400

    Parser not quite working

--- parse_scripts/gem5_to_mcpat.py
+++ parse_scripts/gem5_to_mcpat.py
@@ -3,15 +3,25 @@ import json
 import re
 from xml.etree import ElementTree as ET
 from xml.dom import minidom
+import copy
+import types
 
 bench_path = "/scratch/mhuang_lab/ashar36/dla2/sim/debug_10m_rzheng/MinorCPU/0/0/hetero_power9smt4_TAGE_SC_L_64KB/astar/"
 
-mcpat_template = "template.xml"
+mcpat_template = "alpha_template.xml"
 
 # Don't replace cpus_name for
 # 1. branch predictor
 # 2. caches
+# 3. workload
 cpus_name = 'switch_cpus'
+
+def prettify(elem):
+    """Return a pretty-printed XML string for the Element.
+    """
+    rough_string = ET.tostring(elem, 'utf-8')
+    reparsed = minidom.parseString(rough_string)
+    return reparsed.toprettyxml(indent="  ")
 
 class PIParser(ET.XMLTreeBuilder):
    def __init__(self):
@@ -58,7 +68,6 @@ def readgem5Stats (path):
         if not ignores.match(line):
             count += 1
             statKind = statLine.match(line).group(1)
-            print statKind
             statValue = statLine.match(line).group(2)
             if statValue == 'nan':
                 print "\tWarning (stats): %s is nan. Setting it to 0" % statKind
@@ -70,9 +79,9 @@ def readMcpatTemplate (path):
     global templateMcpat
     templateMcpat = parse(mcpat_template)
 
-def prepareTemplate(outputFile):
-    numCores = len(config["system"]["switch_cpus"])
-    privateL2 = config["system"]["switch_cpus"][0].has_key('l2cache')
+def prepareTemplate (path):
+    numCores = len(config["system"][cpus_name])
+    privateL2 = config["system"]["cpu"][0].has_key('l2cache')
     sharedL2 = config["system"].has_key('l2')
     if privateL2:
         numL2 = numCores
@@ -81,6 +90,17 @@ def prepareTemplate(outputFile):
     else:
         numL2 = 0
     elemCounter = 0
+
+    # Count the number of Inorder and O3 cpus seperately
+    num_inorder = 0
+    num_o3 = 0
+    for coreCounter in range(numCores):
+        if config["system"][cpus_name][coreCounter]["type"] == "MinorCPU":
+            num_inorder += 1
+
+        elif config["system"][cpus_name][coreCounter]["type"] == "DerivO3CPU":
+            num_o3 += 1
+
     root = templateMcpat.getroot()
     for child in root[0][0]:
         elemCounter += 1 # to add elements in correct sequence
@@ -90,25 +110,21 @@ def prepareTemplate(outputFile):
         if child.attrib.get("name") == "number_of_L2s":
             child.attrib['value'] = str(numL2)
         if child.attrib.get("name") == "Private_L2":
-            if sharedL2:
-                Private_L2 = str(0)
-            else:
-                Private_L2 = str(1)
-            child.attrib['value'] = Private_L2
+            child.attrib['value'] = str(sharedL2)
         temp = child.attrib.get('value')
 
         # to consider all the cpus in total cycle calculation
-        if isinstance(temp, basestring) and "cpu." in temp and temp.split('.')[0] == "stats":
-            value = "(" + temp.replace("cpu.", "cpu0.") + ")"
-            for i in range(1, numCores):
-                value = value + " + (" + temp.replace("cpu.", "cpu"+str(i)+".") +")"
-            child.attrib['value'] = value
+        # if isinstance(temp, basestring) and "cpu." in temp and temp.split('.')[0] == "stats":
+        #     value = "(" + temp.replace("cpu.", "cpu0.") + ")"
+        #     for i in range(1, numCores):
+        #         value = value + " + (" + temp.replace("cpu.", "cpu"+str(i)+".") +")"
+        #     child.attrib['value'] = value
 
         # remove a core template element and replace it with number of cores template elements
-        if child.attrib.get("name") == "core":
+        if child.attrib.get("name") == "Minorcore" and num_inorder:
             coreElem = copy.deepcopy(child)
             coreElemCopy = copy.deepcopy(coreElem)
-            for coreCounter in range(numCores):
+            for coreCounter in range(num_inorder):
                 coreElem.attrib["name"] = "core" + str(coreCounter)
                 coreElem.attrib["id"] = "system.core" + str(coreCounter)
                 for coreChild in coreElem:
@@ -120,32 +136,101 @@ def prepareTemplate(outputFile):
                             childValue = "1"
                         else:
                             childValue = "0"
-                    if isinstance(childId, basestring) and "core" in childId:
-                        childId = childId.replace("core", "core" + str(coreCounter))
+                    if isinstance(childId, basestring) and "Minorcore" in childId:
+                        childId = childId.replace("Minorcore", "core" + str(coreCounter))
                     if isinstance(childValue, basestring) and "cpu." in childValue and "stats" in childValue.split('.')[0]:
-                        childValue = childValue.replace("cpu." , "cpu" + str(coreCounter)+ ".")
+                        if "cpu.branchPred." in childValue or "cpu.dcache." in childValue or "cpu.icache." in childValue or "cpu.workload." in childValue:
+                            childValue = childValue.replace("cpu." , "cpu" + str(coreCounter)+ ".")
+                        else:
+                            childValue = childValue.replace("cpu." , cpus_name + str(coreCounter)+ ".")
                     if isinstance(childValue, basestring) and "cpu." in childValue and "config" in childValue.split('.')[0]:
-                        childValue = childValue.replace("cpu." , "cpu." + str(coreCounter)+ ".")
+                        if "cpu.branchPred." in childValue or "cpu.dcache." in childValue or "cpu.icache." in childValue or "cpu.workload." in childValue:
+                            childValue = childValue.replace("cpu." , "cpu."+ str(coreCounter)+ ".")
+                        else:
+                            childValue = childValue.replace("cpu." , cpus_name + "."+ str(coreCounter)+ ".")
                     if len(list(coreChild)) is not 0:
                         for level2Child in coreChild:
                             level2ChildValue = level2Child.attrib.get("value")
                             if isinstance(level2ChildValue, basestring) and "cpu." in level2ChildValue and "stats" in level2ChildValue.split('.')[0]:
-                                level2ChildValue = level2ChildValue.replace("cpu." , "cpu" + str(coreCounter)+ ".")
+                                if "cpu.branchPred." in level2ChildValue or "cpu.dcache." in level2ChildValue or "cpu.icache." in level2ChildValue or "cpu.workload." in level2ChildValue:
+                                    level2ChildValue = level2ChildValue.replace("cpu." , "cpu" + str(coreCounter)+ ".")
+                                else:
+                                    level2ChildValue = level2ChildValue.replace("cpu." , cpus_name + str(coreCounter)+ ".")
                             if isinstance(level2ChildValue, basestring) and "cpu." in level2ChildValue and "config" in level2ChildValue.split('.')[0]:
-                                level2ChildValue = level2ChildValue.replace("cpu." , "cpu." + str(coreCounter)+ ".")
+                                if "cpu.branchPred." in level2ChildValue or "cpu.dcache." in level2ChildValue or "cpu.icache." in level2ChildValue or "cpu.workload." in level2ChildValue:
+                                    level2ChildValue = level2ChildValue.replace("cpu." , "cpu." + str(coreCounter)+ ".")
+                                else:
+                                    level2ChildValue = level2ChildValue.replace("cpu." , cpus_name + "." + str(coreCounter)+ ".")
                             level2Child.attrib["value"] = level2ChildValue
+                    
                     if isinstance(childId, basestring):
                         coreChild.attrib["id"] = childId
                     if isinstance(childValue, basestring):
                         coreChild.attrib["value"] = childValue
+                
                 root[0][0].insert(elemCounter, coreElem)
                 coreElem = copy.deepcopy(coreElemCopy)
                 elemCounter += 1
+            
+            root[0][0].remove(child)
+            elemCounter -= 1
+
+        elif child.attrib.get("name") == "O3core" and num_o3:
+            coreElem = copy.deepcopy(child)
+            coreElemCopy = copy.deepcopy(coreElem)
+            for coreCounter in range(num_inorder, numCores):
+                coreElem.attrib["name"] = "core" + str(coreCounter)
+                coreElem.attrib["id"] = "system.core" + str(coreCounter)
+                for coreChild in coreElem:
+                    childId = coreChild.attrib.get("id")
+                    childValue = coreChild.attrib.get("value")
+                    childName = coreChild.attrib.get("name")
+                    if isinstance(childName, basestring) and childName == "x86":
+                        if config["system"]["cpu"][coreCounter]["isa"][0]["type"] == "X86ISA":
+                            childValue = "1"
+                        else:
+                            childValue = "0"
+                    if isinstance(childId, basestring) and "O3core" in childId:
+                        childId = childId.replace("O3core", "core" + str(coreCounter))
+                    if isinstance(childValue, basestring) and "cpu." in childValue and "stats" in childValue.split('.')[0]:
+                        if "cpu.branchPred." in childValue or "cpu.dcache." in childValue or "cpu.icache." in childValue or "cpu.workload." in childValue:
+                            childValue = childValue.replace("cpu." , "cpu" + str(coreCounter)+ ".")
+                        else:
+                            childValue = childValue.replace("cpu." , cpus_name + str(coreCounter)+ ".")
+                    if isinstance(childValue, basestring) and "cpu." in childValue and "config" in childValue.split('.')[0]:
+                        if "cpu.branchPred." in childValue or "cpu.dcache." in childValue or "cpu.icache." in childValue or "cpu.workload." in childValue:
+                            childValue = childValue.replace("cpu." , "cpu."+ str(coreCounter)+ ".")
+                        else:
+                            childValue = childValue.replace("cpu." , cpus_name + "."+ str(coreCounter)+ ".")
+                    if len(list(coreChild)) is not 0:
+                        for level2Child in coreChild:
+                            level2ChildValue = level2Child.attrib.get("value")
+                            if isinstance(level2ChildValue, basestring) and "cpu." in level2ChildValue and "stats" in level2ChildValue.split('.')[0]:
+                                if "cpu.branchPred." in level2ChildValue or "cpu.dcache." in level2ChildValue or "cpu.icache." in level2ChildValue or "cpu.workload." in level2ChildValue:
+                                    level2ChildValue = level2ChildValue.replace("cpu." , "cpu" + str(coreCounter)+ ".")
+                                else:
+                                    level2ChildValue = level2ChildValue.replace("cpu." , cpus_name + str(coreCounter)+ ".")
+                            if isinstance(level2ChildValue, basestring) and "cpu." in level2ChildValue and "config" in level2ChildValue.split('.')[0]:
+                                if "cpu.branchPred." in level2ChildValue or "cpu.dcache." in level2ChildValue or "cpu.icache." in level2ChildValue or "cpu.workload." in level2ChildValue:
+                                    level2ChildValue = level2ChildValue.replace("cpu." , "cpu." + str(coreCounter)+ ".")
+                                else:
+                                    level2ChildValue = level2ChildValue.replace("cpu." , cpus_name + "." + str(coreCounter)+ ".")
+                            level2Child.attrib["value"] = level2ChildValue
+                    
+                    if isinstance(childId, basestring):
+                        coreChild.attrib["id"] = childId
+                    if isinstance(childValue, basestring):
+                        coreChild.attrib["value"] = childValue
+                
+                root[0][0].insert(elemCounter, coreElem)
+                coreElem = copy.deepcopy(coreElemCopy)
+                elemCounter += 1
+            
             root[0][0].remove(child)
             elemCounter -= 1
 
         # remove a L2 template element and replace it with number of L2 template elements
-        if child.attrib.get("name") == "L2":
+        elif child.attrib.get("name") == "L2":
             if privateL2:
                 l2Elem = copy.deepcopy(child)
                 l2ElemCopy = copy.deepcopy(l2Elem)
@@ -174,9 +259,66 @@ def prepareTemplate(outputFile):
 
     prettify(root)
 
+def getConfigValue (confStr):
+    spltConf = re.split('\.', confStr)
+    currConf = config
+    currHierarchy = ""
+    for x in spltConf:
+        currHierarchy += x
+        if x.isdigit():
+            currConf = currConf[int(x)]
+        elif x in currConf:
+            currConf = currConf[x]
+        currHierarchy += "."
+    return currConf
+
+def dumpMcpatTemplate (path):
+    rootElem = templateMcpat.getroot()
+    configMatch = re.compile(r'config\.([][a-zA-Z0-9_:\.]+)')
+    
+    #replace params with values from the GEM5 config file 
+    for param in rootElem.iter('param'):
+        name = param.attrib['name']
+        value = param.attrib['value']
+        if 'config' in value:
+            allConfs = configMatch.findall(value)
+            for conf in allConfs:
+                confValue = getConfigValue(conf)
+                value = re.sub("config."+ conf, str(confValue), value)
+            if "," in value:
+                exprs = re.split(',', value)
+                for i in range(len(exprs)):
+                    exprs[i] = str(eval(exprs[i]))
+                param.attrib['value'] = ','.join(exprs)
+            else:
+                param.attrib['value'] = str(eval(str(value)))
+    
+    #replace stats with values from the GEM5 stats file 
+    statRe = re.compile(r'stats\.([a-zA-Z0-9_:\.]+)')
+    for stat in rootElem.iter('stat'):
+        name = stat.attrib['name']
+        value = stat.attrib['value']
+        if 'stats' in value:
+            allStats = statRe.findall(value)
+            expr = value
+            for i in range(len(allStats)):
+                if allStats[i] in stats:
+                    expr = re.sub('stats.%s' % allStats[i], stats[allStats[i]], expr)
+                else:
+                    expr = re.sub('stats.%s' % allStats[i], str(0), expr)
+                    print "***WARNING: %s does not exist in stats***" % allStats[i]
+                    print "\t Please use the right stats in your McPAT template file"
+
+            if 'config' not in expr and 'stats' not in expr:
+                stat.attrib['value'] = str(eval(expr))
+
+    templateMcpat.write(path + '/m5out/mcpat_new.xml')
+
 def start (path):
     readgem5Config(path)
     readgem5Stats(path)
     readMcpatTemplate(path)
+    prepareTemplate(path)
+    dumpMcpatTemplate(path)
 
 start(bench_path)
